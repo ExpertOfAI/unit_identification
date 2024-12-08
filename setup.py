b23e8a19289b3b60001f2b4cd7b55bc7161ee182
ExpertOfAI <70627925+ExpertOfAI@users.noreply.github.com>
2024-12-09 00:45:57 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='unit_identification_cpu',
-		version='1.8',
+		version='1.9',
 		description='UOM identification_cpu',
 		url='https://github.com/ExpertOfAI/unit_identification',
 		author='ExpertOfAI1',
@@ -19,5 +19,5 @@ setup(name='unit_identification_cpu',
 		'Operating System :: OS Independent',
 		],
 		python_requires='>=3.6',
-		install_requires = ["quantulum3", "stemming", "cryptography"]
+		install_requires = ["quantulum3", "stemming"]
 		)
